4429924aacac431b04eb75a6fcbc7f5f4606c081
FranciscoJavierdaCostaMillos <javito97x@gmail.com>
2016-10-19 09:13:14 +0200

    Update Boletin4.java

--- src/boletin4/Boletin4.java
+++ src/boletin4/Boletin4.java
@@ -10,10 +10,11 @@ public class Boletin4 {
     
     public static void main(String[] args) {
         Coche coche1 = new Coche();
-        Satelite satelite1 = new Satelite(20.00, 30000.00, 21.0);
+        
+        Satelite satelite1 = new Satelite(15.00, 1000.00, 3.0);
         satelite1.verPosicion();
         
-        Circulo circulo1 =  new Circulo(20);
+        Circulo circulo1 =  new Circulo(56);
         System.out.println(circulo1.calculaLonxitude());
         System.out.println(circulo1.calcularArea());
         
